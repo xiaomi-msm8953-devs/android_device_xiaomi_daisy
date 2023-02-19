2d59a5ba10a078ab8f9dfe7eabfb1938e87f5c0c
Giovanni Ricca <giovanniricca@proton.me>
2023-02-19 14:06:27 +0100

    daisy: Set partitions flag
    Change-Id: I035f4cace2999484df4d8c08ce3e628f79c26daf

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,5 +12,16 @@ include device/xiaomi/msm8953-common/BoardConfigCommon.mk
 # Kernel
 TARGET_KERNEL_CONFIG := daisy_defconfig
 
+# Filesystem
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+BOARD_USES_RECOVERY_AS_BOOT := true
+TARGET_NO_RECOVERY := true
+
+# Partitions
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2684354560
+BOARD_VENDORIMAGE_PARTITION_SIZE := 805306368
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR := vendor
+
 # Inherit the proprietary files
 include vendor/xiaomi/daisy/BoardConfigVendor.mk
