17cdc670513e67c0e01949e09e460d09b08b0051
Michael Bestas <mkbestas@lineageos.org>
2025-02-07 00:36:31 +0200

    daisy: Don't use BSG framework for gpt-utils
    Change-Id: Ia6e5d651560c8ae5186a48bfbb0e30b864150564

--- device.mk
+++ device.mk
@@ -47,6 +47,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti \
     android.hardware.boot-service.qti.recovery
+
+$(call soong_config_set,QTI_GPT_UTILS,USE_BSG_FRAMEWORK,false)
 endif
 
 # Camera
